91361211c2d1973cc8bf58a6dae3264172c5680b
yeaminrabbi <yeaminrabbi308@gmail.com>
2023-06-26 15:23:20 +0600

    Authentication

---
+++ src/views/pages/auth/Login.vue
@@ -0,0 +1,81 @@
+<template>
+
+    <div class="container">
+        <h1 class="">Welcome to Jelly Store</h1>
+        <div>
+
+            <div class="alert alert-primary" role="alert" v-if="notificationStatus">
+                {{ notification }}
+            </div>
+
+            <form @submit.prevent="authenticate">
+                <div class="form-group">
+                    <label class="text-left">Email</label>
+                    <input type="text" v-model="useremail" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                    <label>password</label>
+                    <input type="password" v-model="userpassword" class="form-control" required>
+                </div>
+            
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+        useremail: '',
+        userpassword: '',
+        BASE_URL: window.config.BASE_URL,   
+        
+        notification : '',
+        notificationStatus: false,
+    };
+  },
+  methods: {
+    authenticate(){
+        const authData = {
+            email: this.useremail,
+            password: this.userpassword
+        };
+        
+        axios.post(`${this.BASE_URL}/login`, authData)
+            .then(response => {
+                // Handle the successful response
+   
+                console.log(response.data);
+   
+                // Reset the form fields
+                this.useremail = '';
+                this.userpassword = '';
+
+                //notification message
+                this.notification = response.data.message;
+                this.notificationStatus = true;
+                this.notificationDelay();
+
+            })
+            .catch(error => {
+            // Handle any errors
+                console.error('Error adding product:', error);
+            });
+    },
+
+    notificationDelay() {
+      setTimeout(() => {
+        this.notificationStatus = false;
+    }, 3000); // 3000 milliseconds = 3 seconds
+    }
+
+    
+  }
+};
+</script>
